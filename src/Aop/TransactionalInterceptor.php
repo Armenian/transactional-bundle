271a976591b8b7877ea215846b0dfea90ae29426
david@akopyan.lv <david@akopyan.lv>
2023-08-15 15:51:04 +0300

    - remove logger

--- src/Aop/TransactionalInterceptor.php
+++ src/Aop/TransactionalInterceptor.php
@@ -7,14 +7,13 @@ namespace DMP\TransactionalBundle\Aop;
 use CG\Proxy\MethodInterceptorInterface;
 use CG\Proxy\MethodInvocation;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Throwable;
 
 class TransactionalInterceptor implements MethodInterceptorInterface
 {
 
     public function __construct(
-        private readonly EntityManagerInterface $em, private readonly LoggerInterface $logger)
+        private readonly EntityManagerInterface $em)
     {}
 
     /**
@@ -22,20 +21,14 @@ class TransactionalInterceptor implements MethodInterceptorInterface
      */
     public function intercept(MethodInvocation $invocation): mixed
     {
-        $this->logger->debug('Beginning transaction.', ['method' => $invocation]);
         $this->em->getConnection()->beginTransaction();
         try {
             $return = $invocation->proceed();
-
-            $this->logger->debug('Committing transaction.', ['method' => $invocation]);
-
             $this->em->flush();
             $this->em->getConnection()->commit();
 
             return $return;
         } catch (Throwable $throwable) {
-
-            $this->logger->warning('Rolling back transaction.', ['method' => $invocation]);
             $this->em->getConnection()->rollBack();
 
             throw $throwable;
