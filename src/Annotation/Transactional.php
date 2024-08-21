d7a37b4aed2adc0a7f67317eeb773a040bc8a2e1
David Akopyan <david@akopyan.lv>
2024-08-21 16:57:05 +0300

    SF6.4

--- src/Annotation/Transactional.php
+++ src/Annotation/Transactional.php
@@ -8,6 +8,7 @@ namespace DMP\TransactionalBundle\Annotation;
  * @Annotation
  * @Target({"METHOD", "CLASS"})
  */
+#[\Attribute(\Attribute::IS_REPEATABLE | \Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 class Transactional
 {
 
